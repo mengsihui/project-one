195582f5436766771f0080966eaebd4a81b6995b
mengsihui <m18567893836@163.com>
2024-03-26 17:34:18 +0800

    first

---
+++ src/views/DataSourceView.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import DataSourceLeftPanel from '@/components/DataSourceLeftPanel.vue'
+</script>
+
+<template>
+  <div class="ds-wrapper">
+    <DataSourceLeftPanel />
+  </div>
+</template>
+
+<style scoped>
+.ds-wrapper {
+  display: flex;
+  height: 100%;
+}
+</style>
